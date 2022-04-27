618c5c05e61d32383fe5e4598561362bc150dec9
DELL <DELL@DESKTOP-CG3KOCH>
2022-04-27 23:32:59 +0700

    update

---
+++ .history/app_20220426224022.js
@@ -0,0 +1,20 @@
+$('#multi3').mdbRange({
+    width: '100%',
+    single: {
+        active: true,
+        multi: {
+            active: true,
+            rangeLength: 2
+        },
+    }
+});
+
+// slider
+var counter = 1; 
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 3){
+        counter = 1;
+    }
+},5000);
---
+++ .history/app_20220426225659.js
@@ -0,0 +1,12 @@
+/* $('#multi3').mdbRange({
+    width: '100%',
+    single: {
+        active: true,
+        multi: {
+            active: true,
+            rangeLength: 2
+        },
+    }
+}); */
+
+// slider
